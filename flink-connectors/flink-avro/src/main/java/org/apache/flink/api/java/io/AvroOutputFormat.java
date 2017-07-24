01fd23ffaf5967df93b767ca79c7ec7dbc82f390
aditiverma <averma@branch.io>
2017-07-24 11:00:55 -0700

    avro check

--- flink-connectors/flink-avro/src/main/java/org/apache/flink/api/java/io/AvroOutputFormat.java
+++ flink-connectors/flink-avro/src/main/java/org/apache/flink/api/java/io/AvroOutputFormat.java
@@ -32,6 +32,7 @@ import org.apache.avro.specific.SpecificDatumWriter;
 
 import java.io.IOException;
 import java.io.Serializable;
+import org.apache.avro.generic.GenericDatumWriter;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
 
@@ -127,7 +128,14 @@ public class AvroOutputFormat<E> extends FileOutputFormat<E> implements Serializ
 
 		DatumWriter<E> datumWriter;
 		Schema schema;
-		if (org.apache.avro.specific.SpecificRecordBase.class.isAssignableFrom(avroValueType)) {
+		if (org.apache.avro.generic.GenericData.Record.class == avroValueType) {
+     		 datumWriter = new GenericDatumWriter<E>();
+     		 if (this.userDefinedSchema == null) {
+       		 throw new RuntimeException("schema need to set in GenericRecord");
+      		} else {
+     		   schema = this.userDefinedSchema;
+      	}
+    	} else if (org.apache.avro.specific.SpecificRecordBase.class.isAssignableFrom(avroValueType)) {
 			datumWriter = new SpecificDatumWriter<E>(avroValueType);
 			try {
 				schema = ((org.apache.avro.specific.SpecificRecordBase) avroValueType.newInstance()).getSchema();
